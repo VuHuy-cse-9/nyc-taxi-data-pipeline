9071f81bf9a1e251ba2b12b22da7678c11b4d5aa
huy vu <huy.cse.9@gmail.com>
2026-02-18 18:32:58 +0700

    feat: Update two-stages pipeline for each source

--- airflow/dags/fhvhv_dag.py
+++ airflow/dags/fhvhv_dag.py
@@ -9,7 +9,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id="datamart_fhvhv_dag",
+    dag_id="fhvhv_dag",
     default_args=default_args,
     description="Run fhvhv batch job via Docker",
     start_date=datetime(2026, 1, 1),
@@ -17,8 +17,27 @@ with DAG(
     catchup=False,
     tags=["batch", "docker"],
 ) as dag:
+    
+    # Warehouse job
+    run_warehouse_batch_processor = DockerOperator(
+        task_id="run_warehouse_fhvhv_job",
+        image="doan-batch_processor:latest",
+        api_version="auto",
+        auto_remove="never",
+        command="uv run -m datawarehouse.fhvhv",
+        environment={
+            "MINIO_ENDPOINT": "minio:9000",
+            "MINIO_ACCESS_KEY": "minio_access_key",
+            "MINIO_SECRET_KEY": "minio_secret_key",
+            "DATAMART_ENDPOINT": "datamart_db",
+            "DATAMART_PORT": "5434",
+            "SPARK_MASTER": "spark://spark-master:7077",
+        },
+        docker_url="unix://var/run/docker.sock",  # adjust if remote
+        network_mode="doan_nyc_network",  # change if using custom network
+    )
 
-    run_batch_processor = DockerOperator(
+    run_datamart_batch_processor = DockerOperator(
         task_id="run_datamart_fhvhv_job",
         image="doan-batch_processor:latest",
         api_version="auto",
@@ -36,4 +55,4 @@ with DAG(
         network_mode="doan_nyc_network",  # change if using custom network
     )
 
-    run_batch_processor
+    run_warehouse_batch_processor >> run_datamart_batch_processor
--- airflow/dags/green_taxi_dag.py
+++ airflow/dags/green_taxi_dag.py
@@ -9,7 +9,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id="datamart_green_taxi_dag",
+    dag_id="green_taxi_dag",
     default_args=default_args,
     description="Run green taxi batch job via Docker",
     start_date=datetime(2026, 1, 1),
@@ -18,7 +18,27 @@ with DAG(
     tags=["batch", "docker"],
 ) as dag:
 
-    run_batch_processor = DockerOperator(
+    # Warehouse job
+    run_warehouse_batch_processor = DockerOperator(
+        task_id="run_warehouse_green_taxi_job",
+        image="doan-batch_processor:latest",
+        api_version="auto",
+        auto_remove="never",
+        command="uv run -m datawarehouse.green_taxi",
+        environment={
+            "MINIO_ENDPOINT": "minio:9000",
+            "MINIO_ACCESS_KEY": "minio_access_key",
+            "MINIO_SECRET_KEY": "minio_secret_key",
+            "DATAMART_ENDPOINT": "datamart_db",
+            "DATAMART_PORT": "5434",
+            "SPARK_MASTER": "spark://spark-master:7077",
+        },
+        docker_url="unix://var/run/docker.sock",  # adjust if remote
+        network_mode="doan_nyc_network",  # change if using custom network
+    )
+
+    # Datamart job
+    run_datamart_batch_processor = DockerOperator(
         task_id="run_datamart_green_taxi_job",
         image="doan-batch_processor:latest",
         api_version="auto",
@@ -36,4 +56,4 @@ with DAG(
         network_mode="doan_nyc_network",  # change if using custom network
     )
 
-    run_batch_processor
+    run_warehouse_batch_processor >> run_datamart_batch_processor
