777b0b5c0b05bcc705263aaf642cb982ce0d1f8b
huy vu <huy.cse.9@gmail.com>
2026-02-27 00:13:07 +0700

    feat: Update Trino command to connect to delta lake

--- batch_processing/database/sql/dwh/schema.sql
+++ batch_processing/database/sql/dwh/schema.sql
@@ -1,3 +1,7 @@
 -- CREATE SCHEMA --
-CREATE SCHEMA dwh.nyc_taxi
+CREATE SCHEMA deltalake.nyc_taxi
 WITH (location = 's3a://data-warehouse/nyc_taxi_dataset');
+
+
+-- Register tables in the schema --
+CALL deltalake.system.register_table(schema_name => 'nyc_taxi', table_name => 'green_taxi', table_location => 's3://data-warehouse/nyc_taxi_dataset/green_taxi/07_2025.parquet')
